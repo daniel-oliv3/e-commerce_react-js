1f9d46b6ac8bca137c4158ee9bc74bbd01f9fc81
Daniel Oliveira <danielsapup3@gmail.com>
2022-10-30 13:33:30 -0300

    Create components Hero.jsx and Hero.module.css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
-import Header from "./components/header/Header";
+import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Hero/>
     </div>
   );
 }
---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import css from './Hero.module.css';
+
+const Hero = () => {
+    return (
+        <div>Hero</div>
+    );
+}
+
+export default Hero;
